59eaaafd38857b0b9cc69b9f32c7e1261ca9690a
xinyitan19 <54818568+xinyitan19@users.noreply.github.com>
2019-09-02 19:06:13 -0400

    Add files via upload

---
+++ PersonDriver.java
@@ -0,0 +1,18 @@
+/**
+ * @author Xinyi (Kristen) Tan
+ * @date WHEN DID YOU FINISH THIS
+
+ */
+public class PersonDriver {
+	public static void main(String[] args) {
+		Person p = new Person();
+		Person q = new Person("Sam",21);
+		Person r = new Person("Chris",99);
+
+
+		System.out.println(p);
+		System.out.println(q);
+		System.out.println(r);
+
+	}
+}
